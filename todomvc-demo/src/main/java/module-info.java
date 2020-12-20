49a9c6abdd20b43ed8eb8b640427e890bc3a7961
Falko Schumann <falko.schumann@muspellheim.de>
2020-12-20 23:02:17 +0100

    [App, Demo] Demo als Parameter in App integriert

--- todomvc-demo/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module de.muspellheim.todomvc.demo {
-  requires de.muspellheim.todomvc;
-  requires de.muspellheim.todomvc.backend;
-  requires de.muspellheim.todomvc.frontend;
-  requires static lombok;
-
-  exports de.muspellheim.todomvc.demo;
-}
