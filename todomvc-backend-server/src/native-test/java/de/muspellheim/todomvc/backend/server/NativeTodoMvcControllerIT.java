eeaa1cb7b74e3472dc71e91daeb4fbde8f768688
Falko Schumann <falko.schumann@muspellheim.de>
2021-01-18 22:32:28 +0100

    [Backend Server] Native Test angelegt
    Funktioniert nocht nicht, weil aus dem Unit Test ein Integration Test gemacht werden muss. Das heißt, keine "Innereien" im Test tauschen, wie hier das Repository.

---
+++ todomvc-backend-server/src/native-test/java/de/muspellheim/todomvc/backend/server/NativeTodoMvcControllerIT.java
@@ -0,0 +1,11 @@
+/*
+ * TodoMVC - Backend Server
+ * Copyright (c) 2021 Falko Schumann <falko.schumann@muspellheim.de>
+ */
+
+package de.muspellheim.todomvc.backend.server;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeTodoMvcControllerIT extends TodoMvcControllerTests {}
