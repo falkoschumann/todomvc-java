e089cdfa671860c04b7151191a82430375c1c88d
Falko Schumann <falko.schumann@muspellheim.de>
2021-03-04 19:34:56 +0100

    [Code Style] Unnötigen Import entfernt

--- todomvc-frontend/src/main/java/de/muspellheim/todomvc/frontend/InfoView.java
+++ todomvc-frontend/src/main/java/de/muspellheim/todomvc/frontend/InfoView.java
@@ -13,7 +13,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
